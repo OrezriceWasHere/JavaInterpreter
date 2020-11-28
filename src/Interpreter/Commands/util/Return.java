00ac880bb7732f230b9e79a9a5cd4502d61de060
Or Shachar <orsh>
2020-11-28 12:36:22 +0200

    Added support for return command

--- src/Interpreter/Commands/util/Return.java
+++ src/Interpreter/Commands/util/Return.java
@@ -1,23 +1,57 @@
 package Interpreter.Commands.util;
 
+import Expression.Expression;
+import Interpreter.CalcExpresion;
 import Interpreter.Commands.Exceptions.InvalidArgumentsException;
+import Interpreter.Commands.Exceptions.ParseException;
 import Interpreter.Commands.Fundation.Command;
 
-public class Return<T> extends Command<T> {
+public class Return extends Command<Integer> {
 
-    @Override
-    public T execute() {
-        return null;
+    private Integer returnStatus;
+
+    public static final String RETURN_COMMAND_NAME = "return";
+
+    public Return() {
+        super(RETURN_COMMAND_NAME);
     }
 
     @Override
-    public void setArgs(String[] args) throws InvalidArgumentsException {
-        if(args.length != 2) // args[0] == return && args[1] == return value
-        {
-            throw new InvalidArgumentsException();
+    public Integer execute() {
+        if (returnStatus == null) {
+            throw new VerifyError("Impossible return command: no value set for return");
         }
-        super.setArgs(args);
+        System.exit(returnStatus);
+        return returnStatus;
+    }
 
+    /**
+     * Build a a return command out of string of arguments.
+     * Assumption is args[0] = 'return' and args[1...length] = expression: 3 + 5 - 2 * 8 ....
+     * @param args the arguments of command
+     * @throws InvalidArgumentsException if arguments do not match above order.
+     */
+    @Override
+    public void setArgs(String[] args) throws InvalidArgumentsException {
+        String commandName = getName();
+        if (args.length <= 1) {
+            throw new InvalidArgumentsException("Command " + commandName +
+                    " was not given enough parameters");
+        } else if (!args[0].equals(commandName)) {
+            throw new InvalidArgumentsException("Command " + commandName
+                    + " did not start with command name");
+        }
+        StringBuffer argumentBuffer = new StringBuffer();
+        for (int i = 1; i < args.length; i++) {
+            argumentBuffer.append(args[i]);
+        }
+        String argumentString = argumentBuffer.toString();
+        this.returnStatus = (int) CalcExpresion.calc(argumentString);
+        super.setArgs(args);
+    }
+
+    public Integer getReturnStatus() {
+        return returnStatus;
     }
 }
 
